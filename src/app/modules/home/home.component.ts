05b836d3d08ed0ada2853bee35c98048a827cb21
Ahmedmabrouk8 <a01150502378a@gmail.com>
2025-03-11 13:39:38 +0200

    finshed from Navbar and footer

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { SliderComponent } from "../../components/slider/slider.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, SliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
